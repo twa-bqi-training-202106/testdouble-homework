7d3f33fa0acc9579e9cc29b92bfbf338c2c99b72
Zhaoyu <weixiaobo88@gmail.com>
2021-06-13 16:37:29 +0800

    init commit

---
+++ src/main/java/com/tw/banking/StatementPrinter.java
@@ -0,0 +1,52 @@
+package com.tw.banking;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class StatementPrinter {
+
+    public static final String STATEMENT_HEADER = "DATE | AMOUNT | BALANCE";
+    public static final String SEPARATOR = " | ";
+    private Console console;
+
+    public StatementPrinter(Console console) {
+        this.console = console;
+    }
+
+    public void print(List<Transaction> transactions) {
+        console.printLine(STATEMENT_HEADER);
+        printStatementLines(transactions);
+    }
+
+    private void printStatementLines(List<Transaction> transactions) {
+        List<Transaction> copyOfTransactions = new ArrayList(transactions);
+        printInReverseOrder(generateChronologicalOrderedStatementLines(copyOfTransactions));
+    }
+
+    private void printInReverseOrder(List<String> orderedStatementLines) {
+        Collections.reverse(orderedStatementLines);
+        for (String line : orderedStatementLines) {
+            console.printLine(line);
+        }
+    }
+
+    private List<String> generateChronologicalOrderedStatementLines(List<Transaction> copyOfTransactions) {
+        List<String> lines = new ArrayList<>();
+        Collections.sort(copyOfTransactions);
+        int runningBalance = 0;
+        for (Transaction transaction: copyOfTransactions) {
+            runningBalance+= transaction.amount();
+            lines.add(statementLine(transaction, runningBalance));
+        }
+        return lines;
+    }
+
+    protected String statementLine(Transaction transaction, int runningBalance) {
+        return transaction.date()
+                + SEPARATOR
+                + transaction.amount()
+                + SEPARATOR
+                 + runningBalance;
+    }
+}
---
+++ src/main/java/com/tw/banking/Transaction.java
@@ -0,0 +1,58 @@
+package com.tw.banking;
+
+import org.joda.time.LocalDate;
+import org.joda.time.format.*;
+
+import java.lang.Comparable;
+
+import static com.tw.banking.Clock.DATE_FORMAT;
+
+
+public class Transaction implements Comparable<Transaction>{
+    private String date;
+    private int amount;
+
+    public Transaction(String date, int amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+
+    public String date() {
+        return date;
+    }
+
+    public int amount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transaction that = (Transaction) o;
+
+        if (amount != that.amount) return false;
+        return !(date != null ? !date.equals(that.date) : that.date != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = date != null ? date.hashCode() : 0;
+        result = 31 * result + amount;
+        return result;
+    }
+
+    @Override
+    public int compareTo(Transaction t2) {
+        final DateTimeFormatter dtf = DateTimeFormat.forPattern(DATE_FORMAT);
+        final LocalDate date1 = dtf.parseLocalDate(this.date);
+        final LocalDate date2 = dtf.parseLocalDate(t2.date);
+        if (date1.isBefore(date2)) {
+            return -1;
+        }
+        return 1;
+    }
+
+}
